8ec65e665552d6d186097f8d0ce5619727c3eed2
태고밍 <68893329+TaeKyeong-coder@users.noreply.github.com>
2022-06-13 18:14:02 +0900

    [김태경/220613/1813] 빠진 파일 추가

---
+++ app/src/main/java/com/lge/support/second/application/main/view/adapter/questionModel.kt
@@ -0,0 +1,9 @@
+package com.lge.support.second.application.main.view.adapter
+
+class questionModel {
+    var question:String? = null
+
+    constructor(question:String) {
+        this.question = question
+    }
+}
