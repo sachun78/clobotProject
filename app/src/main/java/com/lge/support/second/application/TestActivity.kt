31fd60d2688adb5325904c73d94252e5d62cb2b8
hjbae <sachun78@naver.com>
2022-06-15 16:37:18 +0900

    * fixed: test app ui

--- app/src/main/java/com/lge/support/second/application/TestActivity.kt
+++ app/src/main/java/com/lge/support/second/application/TestActivity.kt
@@ -3,18 +3,53 @@ package com.lge.support.second.application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import androidx.lifecycle.Observer
-import com.lge.support.second.application.MainActivity.Companion.tkTestViewModel
-import com.lge.support.second.application.main.model.ActionType
+import android.widget.*
+import com.lge.robot.platform.PlatformUtil.context
+import com.lge.robot.platform.navigation.navigation.NavigationManager
+import com.lge.robot.platform.power.PowerManager
+import com.lge.robot.platform.util.poi.data.POI
+import com.lge.support.second.application.main.managers.robot.NavigationManagerInstance
+import com.lge.support.second.application.main.managers.robot.PowerManagerInstance
 import com.lge.support.second.application.main.model.TkTestViewModel
+import com.lge.support.second.application.main.poi.PoiDbManager
 
 class TestActivity : AppCompatActivity() {
     lateinit var tkTestViewModel2: TkTestViewModel
 
+    private lateinit var mNavigationManager: NavigationManager
+    private lateinit var listView: ListView
+    private var mInitPos: POI? = null
+    private var mPOIs: ArrayList<POI>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+
+        mNavigationManager = NavigationManagerInstance.instance.getNavigationManager()
+
+        mInitPos = context?.let { PoiDbManager(it).getInitPosition() }
+        mPOIs  = context?.let { PoiDbManager(it).getAllPoi() }
+
+        listView = findViewById<ListView>(R.id.listview)
+        var listItems = arrayListOf<POI>()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
+        listView.adapter = adapter
+
+        findViewById<Button>(R.id.btn_activation).setOnClickListener {
+            var power: PowerManager = PowerManagerInstance.instance.getPowerManager()
+            power.robotActivation()
+            Toast.makeText(this, "Activation !!!", Toast.LENGTH_SHORT).show()
+            Log.i("HJBAE", "powerMode: ${power.getPowerMode()}")
+        }
+
+        findViewById<Button>(R.id.btn_pois).setOnClickListener {
+            listItems = mPOIs ?: arrayListOf<POI>()
+            adapter.notifyDataSetChanged()
+        }
+
+        listView.setOnItemClickListener { parent, view, pos, id ->
+            Toast.makeText(this, "pos:${pos}, id:${id} !!!", Toast.LENGTH_SHORT).show()
+        }
 
 //        tkTestViewModel = MainActivity.tkTestViewModel
 ////        참고 private val viewModel: NavigationModel by viewModels()
